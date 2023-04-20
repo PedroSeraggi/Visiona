d9a5ee264bdb10b374048f85743c85d57de6bd0d
PedroSeraggi <pedroseraggi09@gmail.com>
2023-04-20 16:35:12 -0300

    Alteração na tabela e barra de pesquisa.

--- Sprints/2_Sprint/codigo/src/components/list/list.js
+++ Sprints/2_Sprint/codigo/src/components/list/list.js
@@ -1,7 +1,7 @@
 import { FaTrash, FaEye, FaPen } from 'react-icons/fa';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../../App.css'
+import '../../App.css';
 import UserModal from './view';
 import EditUserModal from './edit';
 
@@ -10,6 +10,7 @@ function List() {
   const [selectedUser, setSelectedUser] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     async function fetchData() {
@@ -20,53 +21,70 @@ function List() {
   }, []);
 
   const getStatus = (status) => {
-    return status ? "ativado" : "desativado";
-  }
+    return status ? 'Ativo' : 'Inativo';
+  };
 
   const handleDelete = async (id) => {
     const confirmDelete = window.confirm('Deseja realmente excluir o usuário?');
     if (confirmDelete) {
       try {
         await axios.delete(`http://localhost:3333/users/${id}`);
-        setUsers(users.filter(user => user.id !== id));
+        setUsers(users.filter((user) => user.id !== id));
         alert('Usuário excluído com sucesso!');
       } catch (error) {
         alert('Não foi possível excluir o usuário');
       }
     }
-  }
-
+  };
 
   const handleSaveUser = async (user) => {
     try {
       await axios.put(`http://localhost:3333/users/${user.id}`, user);
-      setUsers(users.map(u => u.id === user.id ? user : u));
+      setUsers(users.map((u) => (u.id === user.id ? user : u)));
       alert('Usuário atualizado com sucesso!');
     } catch (error) {
-      console.log(error)
+      console.log(error);
       alert('Não foi possível atualizar o usuário');
     }
     setShowEditModal(false);
-  }
-
+  };
 
   const handleViewUser = (user) => {
     setSelectedUser(user);
     setShowModal(true);
-  }
+  };
 
   const handleCloseModal = () => {
     setSelectedUser(null);
     setShowModal(false);
-  }
+  };
 
   const handleEditUser = (user) => {
     setSelectedUser(user);
     setShowEditModal(true);
-  }
+  };
 
+  const filteredUsers = users.filter((user) =>
+    user.nome.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const sortedUsers = [...filteredUsers].sort((a, b) => {
+    if (a.status && !b.status) return -1; // a está ativo, b está inativo
+    if (!a.status && b.status) return 1; // a está inativo, b está ativo
+    return 0; // a e b têm o mesmo status
+  });
+  
   return (
-    <div className='listContent'>
+    <div className="listContent">
+      <div className="BarraPesq">
+        <input
+          type="text"
+          placeholder="Pesquisar por nome"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="Pesquisa"
+        />
+      </div>
       <table>
         <thead>
           <tr>
@@ -78,22 +96,28 @@ function List() {
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
+          {sortedUsers.map((user) => (
+            <tr key={user.id} className={user.status ? "" : "inativo"}>
               <td>{user.id}</td>
               <td>{user.nome}</td>
               <td>{user.email}</td>
-              <td>{user.status ? 'Ativo' : 'Inativo'}</td>
+              <td>{getStatus(user.status)}</td>
               <td>
-                <button onClick={() => handleViewUser(user)}><FaEye /> </button>
-                <button onClick={() => handleDelete(user.id)}><FaTrash /></button>
-                <button onClick={() => handleEditUser(user)}><FaPen /></button>
+                <button onClick={() => handleViewUser(user)}>
+                  <FaEye />{" "}
+                </button>
+                <button onClick={() => handleDelete(user.id)}>
+                  <FaTrash />
+                </button>
+                <button onClick={() => handleEditUser(user)}>
+                  <FaPen />
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-
+  
       {showModal && (
         <UserModal
           isOpen={showModal}
@@ -101,7 +125,7 @@ function List() {
           onClose={handleCloseModal}
         />
       )}
-
+  
       {showEditModal && (
         <EditUserModal
           isOpen={showEditModal}
@@ -112,6 +136,7 @@ function List() {
       )}
     </div>
   );
+  
 }
 
 export default List;
